9081b494d20d4899697c671004758b4ba9e04a48
Krikor Herlopian <kherl1@Unh.newhaven.edu>
2019-11-23 17:50:05 -0500

    tests

--- app/src/test/java/edu/newhaven/krikorherlopian/android/vproperty/CommonKtTest.kt
+++ app/src/test/java/edu/newhaven/krikorherlopian/android/vproperty/CommonKtTest.kt
@@ -7,31 +7,34 @@ import org.junit.Test
 
 class CommonKtTest {
 
+    val tokyo =
+        Property("Tokyo House", address = Address(longitude = "139.691711", latitude = "35.689487"))
+    var newYork =
+        Property("NY House", address = Address(longitude = "-73.986572", latitude = "40.742748"))
+    val berlin =
+        Property("Berlin House", address = Address(longitude = "13.404954", latitude = "52.520008"))
+    var newHaven =
+        Property("NH House", address = Address(longitude = "-72.928080", latitude = "41.304670"))
+    var losAngeles =
+        Property("LA House", address = Address(longitude = "-118.496475", latitude = "34.0522"))
+    var boston = Property(
+        "Boston House",
+        address = Address(longitude = "-71.057083", latitude = "42.361145")
+    )
+    val arizona =
+        Property("AZ House", address = Address(longitude = "-111.0937", latitude = "34.0489"))
+    val florida =
+        Property("FL House", address = Address(longitude = "-81.5158", latitude = "27.6648"))
+
     @Test
     fun sortByDistance_test_success() {
-        var newYork = Property(
-            "NY House",
-            address = Address(longitude = "-73.986572", latitude = "40.742748")
-        )
-        var newHaven = Property(
-            "NH House",
-            address = Address(longitude = "-72.928080", latitude = "41.304670")
-        )
-        var losAngeles =
-            Property("LA House", address = Address(longitude = "-118.496475", latitude = "34.0522"))
-        var boston = Property(
-            "Boston House",
-            address = Address(longitude = "-71.057083", latitude = "42.361145")
-        )
-        val arizona =
-            Property("AZ House", address = Address(longitude = "-111.0937", latitude = "34.0489"))
-        val florida =
-            Property("FL House", address = Address(longitude = "-81.5158", latitude = "27.6648"))
 
         val propertyListActual: MutableList<Any> = mutableListOf(
+            tokyo,
             newYork,
             newHaven,
             losAngeles,
+            berlin,
             boston,
             arizona,
             florida
@@ -42,54 +45,13 @@ class CommonKtTest {
             boston,
             florida,
             arizona,
-            losAngeles
+            losAngeles,
+            berlin,
+            tokyo
         )
         //my current location 300 boston post road new haven connecticut
         //41.287160 latitude, -72.657680 longitude
-        val result = sortByDistance(propertyListActual, "-72.657680", "41.287160 ")
-        assertEquals(propertyListExpected.toList(), propertyListActual.toList())
-    }
-
-    @Test
-    fun sortByDistance_test_fail() {
-        var newYork = Property(
-            "NY House",
-            address = Address(longitude = "-73.986572", latitude = "40.742748")
-        )
-        var newHaven = Property(
-            "NH House",
-            address = Address(longitude = "-72.928080", latitude = "41.304670")
-        )
-        var losAngeles =
-            Property("LA House", address = Address(longitude = "-118.496475", latitude = "34.0522"))
-        var boston = Property(
-            "Boston House",
-            address = Address(longitude = "-71.057083", latitude = "42.361145")
-        )
-        val arizona =
-            Property("AZ House", address = Address(longitude = "-111.0937", latitude = "34.0489"))
-        val florida =
-            Property("FL House", address = Address(longitude = "-81.5158", latitude = "27.6648"))
-
-        val propertyListActual: MutableList<Any> = mutableListOf(
-            newYork,
-            newHaven,
-            losAngeles,
-            boston,
-            arizona,
-            florida
-        )
-        val propertyListExpected: MutableList<Any> = mutableListOf(
-            newYork,
-            newHaven,
-            losAngeles,
-            boston,
-            arizona,
-            florida
-        )
-        //my current location 300 boston post road new haven connecticut
-        //41.287160 latitude, -72.657680 longitude
-        val result = sortByDistance(propertyListActual, "-72.657680", "41.287160 ")
+        sortByDistance(propertyListActual, "-72.657680", "41.287160 ")
         assertEquals(propertyListExpected.toList(), propertyListActual.toList())
     }
 }
