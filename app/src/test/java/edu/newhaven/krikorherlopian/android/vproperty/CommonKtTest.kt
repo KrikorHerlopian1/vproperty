02d1b35f8ad355f1d6c33e2105cf143f5c0534d6
Krikor Herlopian <kherl1@Unh.newhaven.edu>
2019-11-23 17:23:19 -0500

    Add more tests

--- app/src/test/java/edu/newhaven/krikorherlopian/android/vproperty/CommonKtTest.kt
+++ app/src/test/java/edu/newhaven/krikorherlopian/android/vproperty/CommonKtTest.kt
@@ -9,16 +9,18 @@ class CommonKtTest {
 
     @Test
     fun sortByDistance_test() {
-
-
         var newYork = Address(longitude = "-73.986572", latitude = "40.742748")
         var newHaven = Address(longitude = "-72.928080", latitude = "41.304670")
         var losAngeles = Address(longitude = "-118.496475", latitude = "34.0522")
         var boston = Address(longitude = "-71.057083", latitude = "42.361145")
         val arizona = Address(longitude = "-111.0937", latitude = "34.0489")
+        val florida = Address(longitude = "-81.5158", latitude = "27.6648")
+
+
         val propertyListActual: MutableList<Any> = mutableListOf(
             Property("NY House", newYork),
             Property("NH House", newHaven),
+            Property("Florida House", florida),
             Property("LA House", losAngeles),
             Property("BOSTON House", boston),
             Property("AZ House", arizona)
@@ -27,6 +29,7 @@ class CommonKtTest {
             Property("NH House", newHaven, distance = 14.088658705148184),
             Property("NY House", newYork, distance = 78.83144221662072),
             Property("BOSTON House", boston, distance = 110.89273864552389),
+            Property("Florida House", florida, distance = 1066.4791337229924),
             Property("AZ House", arizona, distance = 2142.277482099129),
             Property("LA House", losAngeles, distance = 2526.2674284586433)
         )
