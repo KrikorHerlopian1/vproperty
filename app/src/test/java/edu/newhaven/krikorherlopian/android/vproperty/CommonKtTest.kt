e9f9007b8fcae649e8ac5abed39a7e9df9d83d59
Krikor Herlopian <kherl1@Unh.newhaven.edu>
2019-11-23 17:16:17 -0500

    add more tests

--- app/src/test/java/edu/newhaven/krikorherlopian/android/vproperty/CommonKtTest.kt
+++ app/src/test/java/edu/newhaven/krikorherlopian/android/vproperty/CommonKtTest.kt
@@ -17,18 +17,18 @@ class CommonKtTest {
         var boston = Address(longitude = "-71.057083", latitude = "42.361145")
         val arizona = Address(longitude = "-111.0937", latitude = "34.0489")
         val propertyListActual: MutableList<Any> = mutableListOf(
-            Property("house 1", newYork),
-            Property("house 2", newHaven),
-            Property("house 3", losAngeles),
-            Property("house 4", boston),
-            Property("house 5", arizona)
+            Property("NY House", newYork),
+            Property("NH House", newHaven),
+            Property("LA House", losAngeles),
+            Property("BOSTON House", boston),
+            Property("AZ House", arizona)
         )
         val propertyListExpected: MutableList<Any> = mutableListOf(
-            Property("house 2", newHaven, distance = 14.088658705148184),
-            Property("house 1", newYork, distance = 78.83144221662072),
-            Property("house 4", boston, distance = 110.89273864552389),
-            Property("house 5", arizona, distance = 2142.277482099129),
-            Property("house 3", losAngeles, distance = 2526.2674284586433)
+            Property("NH House", newHaven, distance = 14.088658705148184),
+            Property("NY House", newYork, distance = 78.83144221662072),
+            Property("BOSTON House", boston, distance = 110.89273864552389),
+            Property("AZ House", arizona, distance = 2142.277482099129),
+            Property("LA House", losAngeles, distance = 2526.2674284586433)
         )
         //my current location 300 boston post road new haven connecticut
         //41.287160 latitude, -72.657680 longitude
