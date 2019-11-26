271051ced78fb1e1f6ac175578c556ab61f54132
Krikor Herlopian <kherl1@Unh.newhaven.edu>
2019-11-26 10:33:10 -0500

    views

--- app/src/main/java/edu/newhaven/krikorherlopian/android/vproperty/step/StepBuildingDetails.kt
+++ app/src/main/java/edu/newhaven/krikorherlopian/android/vproperty/step/StepBuildingDetails.kt
@@ -23,7 +23,7 @@ import kotlinx.android.synthetic.main.parking.view.*
 class StepBuildingDetails(context: Context, listener: OnNavigationBarListener, var prop: Property) :
     FrameLayout(context),
     Step {
-    internal var ms: View? = null
+    internal var view: View? = null
     @Nullable
     private var onNavigationBarListener: OnNavigationBarListener? = null
 
@@ -45,59 +45,61 @@ class StepBuildingDetails(context: Context, listener: OnNavigationBarListener, v
     }
 
     private fun init(context: Context, listener: OnNavigationBarListener) {
-        val v = LayoutInflater.from(context)
+        val layoutView = LayoutInflater.from(context)
             .inflate(R.layout.fragment_step_building_details, this, true)
         try {
             onNavigationBarListener = listener
-            ms = v
+            view = layoutView
 
-            ms?.brick!!.isChecked = prop.buildingDetails.exterior.brick
-            ms?.cement!!.isChecked = prop.buildingDetails.exterior.cement
-            ms?.composition!!.isChecked = prop.buildingDetails.exterior.composition
-            ms?.metal!!.isChecked = prop.buildingDetails.exterior.metal
-            ms?.shingle!!.isChecked = prop.buildingDetails.exterior.shingle
-            ms?.stone!!.isChecked = prop.buildingDetails.exterior.stone
-            ms?.stucco!!.isChecked = prop.buildingDetails.exterior.stucco
-            ms?.vinyl!!.isChecked = prop.buildingDetails.exterior.vinyl
-            ms?.wood!!.isChecked = prop.buildingDetails.exterior.wood
-            ms?.wood_products!!.isChecked = prop.buildingDetails.exterior.woodProducts
-            ms?.other!!.isChecked = prop.buildingDetails.exterior.other
+            view?.brick!!.isChecked = prop.buildingDetails.exterior.brick
+            view?.cement!!.isChecked = prop.buildingDetails.exterior.cement
+            view?.composition!!.isChecked = prop.buildingDetails.exterior.composition
+            view?.metal!!.isChecked = prop.buildingDetails.exterior.metal
+            view?.shingle!!.isChecked = prop.buildingDetails.exterior.shingle
+            view?.stone!!.isChecked = prop.buildingDetails.exterior.stone
+            view?.stucco!!.isChecked = prop.buildingDetails.exterior.stucco
+            view?.vinyl!!.isChecked = prop.buildingDetails.exterior.vinyl
+            view?.wood!!.isChecked = prop.buildingDetails.exterior.wood
+            view?.wood_products!!.isChecked = prop.buildingDetails.exterior.woodProducts
+            view?.other!!.isChecked = prop.buildingDetails.exterior.other
 
-            ms?.basketball_court!!.isChecked =
+            view?.basketball_court!!.isChecked =
                 prop.buildingDetails.buildingAminities.basketballCourt
-            ms?.disabled_access!!.isChecked = prop.buildingDetails.buildingAminities.disabledAccess
-            ms?.doorman!!.isChecked = prop.buildingDetails.buildingAminities.doorman
-            ms?.elevator!!.isChecked = prop.buildingDetails.buildingAminities.elevator
-            ms?.fitness_center!!.isChecked = prop.buildingDetails.buildingAminities.fitnessCenter
-            ms?.sports_court!!.isChecked = prop.buildingDetails.buildingAminities.sportsCourt
-            ms?.storage!!.isChecked = prop.buildingDetails.buildingAminities.storage
-            ms?.tennis_court!!.isChecked = prop.buildingDetails.buildingAminities.tennisCourt
-            ms?.near_transportation!!.isChecked =
+            view?.disabled_access!!.isChecked =
+                prop.buildingDetails.buildingAminities.disabledAccess
+            view?.doorman!!.isChecked = prop.buildingDetails.buildingAminities.doorman
+            view?.elevator!!.isChecked = prop.buildingDetails.buildingAminities.elevator
+            view?.fitness_center!!.isChecked = prop.buildingDetails.buildingAminities.fitnessCenter
+            view?.sports_court!!.isChecked = prop.buildingDetails.buildingAminities.sportsCourt
+            view?.storage!!.isChecked = prop.buildingDetails.buildingAminities.storage
+            view?.tennis_court!!.isChecked = prop.buildingDetails.buildingAminities.tennisCourt
+            view?.near_transportation!!.isChecked =
                 prop.buildingDetails.buildingAminities.nearTransportation
 
-            ms?.balcony_patio!!.isChecked = prop.buildingDetails.outdoorAminities.balconyPatio
-            ms?.lawn!!.isChecked = prop.buildingDetails.outdoorAminities.lawn
-            ms?.barbecue_area!!.isChecked = prop.buildingDetails.outdoorAminities.barbecueArea
-            ms?.pond!!.isChecked = prop.buildingDetails.outdoorAminities.pond
-            ms?.deck!!.isChecked = prop.buildingDetails.outdoorAminities.deck
-            ms?.pool!!.isChecked = prop.buildingDetails.outdoorAminities.pool
-            ms?.dock!!.isChecked = prop.buildingDetails.outdoorAminities.dock
-            ms?.porch!!.isChecked = prop.buildingDetails.outdoorAminities.porch
-            ms?.fenced_yard!!.isChecked = prop.buildingDetails.outdoorAminities.fencedYard
-            ms?.rv_parking!!.isChecked = prop.buildingDetails.outdoorAminities.rvParking
-            ms?.garden!!.isChecked = prop.buildingDetails.outdoorAminities.garden
-            ms?.sauna!!.isChecked = prop.buildingDetails.outdoorAminities.sauna
-            ms?.greenhouse!!.isChecked = prop.buildingDetails.outdoorAminities.greenHouse
-            ms?.sprinkler_system!!.isChecked = prop.buildingDetails.outdoorAminities.sprinkerlSystem
-            ms?.hottubspa!!.isChecked = prop.buildingDetails.outdoorAminities.hotTubSpa
-            ms?.waterfront!!.isChecked = prop.buildingDetails.outdoorAminities.waterfront
+            view?.balcony_patio!!.isChecked = prop.buildingDetails.outdoorAminities.balconyPatio
+            view?.lawn!!.isChecked = prop.buildingDetails.outdoorAminities.lawn
+            view?.barbecue_area!!.isChecked = prop.buildingDetails.outdoorAminities.barbecueArea
+            view?.pond!!.isChecked = prop.buildingDetails.outdoorAminities.pond
+            view?.deck!!.isChecked = prop.buildingDetails.outdoorAminities.deck
+            view?.pool!!.isChecked = prop.buildingDetails.outdoorAminities.pool
+            view?.dock!!.isChecked = prop.buildingDetails.outdoorAminities.dock
+            view?.porch!!.isChecked = prop.buildingDetails.outdoorAminities.porch
+            view?.fenced_yard!!.isChecked = prop.buildingDetails.outdoorAminities.fencedYard
+            view?.rv_parking!!.isChecked = prop.buildingDetails.outdoorAminities.rvParking
+            view?.garden!!.isChecked = prop.buildingDetails.outdoorAminities.garden
+            view?.sauna!!.isChecked = prop.buildingDetails.outdoorAminities.sauna
+            view?.greenhouse!!.isChecked = prop.buildingDetails.outdoorAminities.greenHouse
+            view?.sprinkler_system!!.isChecked =
+                prop.buildingDetails.outdoorAminities.sprinkerlSystem
+            view?.hottubspa!!.isChecked = prop.buildingDetails.outdoorAminities.hotTubSpa
+            view?.waterfront!!.isChecked = prop.buildingDetails.outdoorAminities.waterfront
 
-            ms?.carport!!.isChecked = prop.buildingDetails.parking.carport
-            ms?.on_street!!.isChecked = prop.buildingDetails.parking.onStreet
-            ms?.off_street!!.isChecked = prop.buildingDetails.parking.offStreet
-            ms?.garage_attached!!.isChecked = prop.buildingDetails.parking.garageAttached
-            ms?.garage_detached!!.isChecked = prop.buildingDetails.parking.garageDetached
-            ms?.none!!.isChecked = prop.buildingDetails.parking.none
+            view?.carport!!.isChecked = prop.buildingDetails.parking.carport
+            view?.on_street!!.isChecked = prop.buildingDetails.parking.onStreet
+            view?.off_street!!.isChecked = prop.buildingDetails.parking.offStreet
+            view?.garage_attached!!.isChecked = prop.buildingDetails.parking.garageAttached
+            view?.garage_detached!!.isChecked = prop.buildingDetails.parking.garageDetached
+            view?.none!!.isChecked = prop.buildingDetails.parking.none
 
         } catch (e: Exception) {
         }
@@ -108,41 +110,41 @@ class StepBuildingDetails(context: Context, listener: OnNavigationBarListener, v
         try {
 
             var exterior: Exterior = Exterior(
-                ms?.brick!!.isChecked, ms?.cement!!.isChecked,
-                ms?.composition!!.isChecked, ms?.metal!!.isChecked,
-                ms?.shingle!!.isChecked, ms?.stone!!.isChecked,
-                ms?.stucco!!.isChecked, ms?.vinyl!!.isChecked,
-                ms?.wood!!.isChecked, ms?.wood_products!!.isChecked,
-                ms?.other!!.isChecked
+                view?.brick!!.isChecked, view?.cement!!.isChecked,
+                view?.composition!!.isChecked, view?.metal!!.isChecked,
+                view?.shingle!!.isChecked, view?.stone!!.isChecked,
+                view?.stucco!!.isChecked, view?.vinyl!!.isChecked,
+                view?.wood!!.isChecked, view?.wood_products!!.isChecked,
+                view?.other!!.isChecked
             )
 
             var buildingAminities = BuildingAminities(
-                ms?.basketball_court!!.isChecked,
-                ms?.disabled_access!!.isChecked,
-                ms?.doorman!!.isChecked,
-                ms?.elevator!!.isChecked,
-                ms?.fitness_center!!.isChecked,
-                ms?.sports_court!!.isChecked,
-                ms?.storage!!.isChecked,
-                ms?.tennis_court!!.isChecked,
-                ms?.near_transportation!!.isChecked
+                view?.basketball_court!!.isChecked,
+                view?.disabled_access!!.isChecked,
+                view?.doorman!!.isChecked,
+                view?.elevator!!.isChecked,
+                view?.fitness_center!!.isChecked,
+                view?.sports_court!!.isChecked,
+                view?.storage!!.isChecked,
+                view?.tennis_court!!.isChecked,
+                view?.near_transportation!!.isChecked
             )
 
             var outdoorAminities = OutdoorAminities(
-                ms?.balcony_patio!!.isChecked, ms?.lawn!!.isChecked,
-                ms?.barbecue_area!!.isChecked, ms?.pond!!.isChecked,
-                ms?.deck!!.isChecked, ms?.pool!!.isChecked,
-                ms?.dock!!.isChecked, ms?.porch!!.isChecked,
-                ms?.fenced_yard!!.isChecked, ms?.rv_parking!!.isChecked,
-                ms?.garden!!.isChecked, ms?.sauna!!.isChecked,
-                ms?.greenhouse!!.isChecked, ms?.sprinkler_system!!.isChecked,
-                ms?.hottubspa!!.isChecked, ms?.waterfront!!.isChecked
+                view?.balcony_patio!!.isChecked, view?.lawn!!.isChecked,
+                view?.barbecue_area!!.isChecked, view?.pond!!.isChecked,
+                view?.deck!!.isChecked, view?.pool!!.isChecked,
+                view?.dock!!.isChecked, view?.porch!!.isChecked,
+                view?.fenced_yard!!.isChecked, view?.rv_parking!!.isChecked,
+                view?.garden!!.isChecked, view?.sauna!!.isChecked,
+                view?.greenhouse!!.isChecked, view?.sprinkler_system!!.isChecked,
+                view?.hottubspa!!.isChecked, view?.waterfront!!.isChecked
             )
 
             var parking: Parking = Parking(
-                ms?.carport!!.isChecked, ms?.on_street!!.isChecked,
-                ms?.off_street!!.isChecked, ms?.garage_attached!!.isChecked,
-                ms?.garage_detached!!.isChecked, ms?.none!!.isChecked
+                view?.carport!!.isChecked, view?.on_street!!.isChecked,
+                view?.off_street!!.isChecked, view?.garage_attached!!.isChecked,
+                view?.garage_detached!!.isChecked, view?.none!!.isChecked
             )
 
             var buildingDetails: BuildingDetails =
