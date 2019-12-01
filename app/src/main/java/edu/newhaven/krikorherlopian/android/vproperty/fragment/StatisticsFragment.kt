ca12055943076911a41c45c6f5cf60e705f63fb0
Krikor Herlopian <kherl1@Unh.newhaven.edu>
2019-12-01 11:30:17 -0500

    fix navigation

--- app/src/main/java/edu/newhaven/krikorherlopian/android/vproperty/fragment/StatisticsFragment.kt
+++ app/src/main/java/edu/newhaven/krikorherlopian/android/vproperty/fragment/StatisticsFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.android.material.tabs.TabLayout
 import edu.newhaven.krikorherlopian.android.vproperty.R
 import edu.newhaven.krikorherlopian.android.vproperty.adapter.StatisticsAdapter
 import edu.newhaven.krikorherlopian.android.vproperty.fragmentActivityCommunication
@@ -23,7 +22,6 @@ class StatisticsFragment : Fragment() {
             StatisticsAdapter(activity!!, childFragmentManager)
         root?.view_pager?.adapter = statisticsAdapter
         root?.tabs?.setupWithViewPager(root?.view_pager!!)
-        root!!.tabs?.tabMode = TabLayout.MODE_FIXED
         fragmentActivityCommunication!!.hideShowMenuItems(false)
         return root
     }
